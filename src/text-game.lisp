6bf293fda31d9c150fc3f460f2b26ba833f037fb
色とりどりの <yutsuro@gmail.com>
2014-02-06 11:56:48 +0100

    cl: indentation change (tabs=>spaces)

--- src/text-game.lisp
+++ src/text-game.lisp
@@ -1,22 +1,22 @@
 ; location nodes
 (defparameter *nodes* '(
-	(living-room (you are in the living-room.
-				  a wizard is snoring loudly on the couch.))
-	(garden      (you are in a beautiful garden.
-		          there is a well in front of you.))
-	(attic       (you are in the attic.
-				  there is a giant welding torch in the corner.))))
+    (living-room (you are in the living-room.
+                  a wizard is snoring loudly on the couch.))
+    (garden      (you are in a beautiful garden.
+                  there is a well in front of you.))
+    (attic       (you are in the attic.
+                  there is a giant welding torch in the corner.))))
 
 ; Paths - edges
 (defparameter *edges* '(
-	(living-room (garden west door)
-				 (attic upstairs ladder))
-	(garden      (living-room east door))
-	(attic (living-room downstairs ladder))))
+    (living-room (garden west door)
+                  (attic upstairs ladder))
+    (garden      (living-room east door))
+    (attic (living-room downstairs ladder))))
 
 ; assoc for finding correct item in list
 (defun describe-location (location nodes)
-	(cadr (assoc location nodes)))
+    (cadr (assoc location nodes)))
 ; example usage:
 ;(describe-location 'living-room *nodes*)
 
